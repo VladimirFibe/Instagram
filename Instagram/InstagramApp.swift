34fa3d61e291d5cab2e5d9d70de267ab1aadb217
Vladimir Fibe <motiw@icloud.com>
2023-09-20 22:03:48 +0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Vladimir Fibe on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
