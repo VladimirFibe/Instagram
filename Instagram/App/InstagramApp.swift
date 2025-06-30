36ae9cb1ace2e2c52ec12a642eec004d10ca378b
Vladimir <motiw@me.com>
2025-06-30 15:44:14 +0500

    Tab Bar Setup

--- Instagram/App/InstagramApp.swift
+++ Instagram/App/InstagramApp.swift
@@ -1,17 +1,10 @@
-//
-//  InstagramApp.swift
-//  Instagram
-//
-//  Created by Almaty on 30.06.2025.
-//
-
 import SwiftUI
 
 @main
 struct InstagramApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
--- Instagram/Core/Auth/ContentView.swift
+++ Instagram/Core/Auth/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Instagram
-//
-//  Created by Almaty on 30.06.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
