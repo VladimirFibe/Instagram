c5385fb9b362a04493c5ff71653eeac86b5f9163
Vladimir <motiw@me.com>
2025-06-30 14:55:33 +0500

    start

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Almaty on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
