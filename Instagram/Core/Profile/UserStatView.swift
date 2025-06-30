43b3c9f16668c7138f13f11c504d70f739d14eae
Vladimir <motiw@me.com>
2025-06-30 17:21:55 +0500

    UserStatView

---
+++ Instagram/Core/Profile/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  Instagram
+//
+//  Created by Almaty on 30.06.2025.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
