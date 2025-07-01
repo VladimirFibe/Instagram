33438e83b0587be91dbc08ec07bbf11b2589d2a0
Vladimir <motiw@me.com>
2025-07-01 05:25:23 +0500

    ProfileView

--- Instagram/Core/Profile/ProfileView.swift
+++ Instagram/Core/Profile/ProfileView.swift
@@ -1,16 +1,67 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    private let gridItem: [GridItem] = Array(repeating: .init(.flexible(), spacing: 1), count: 3)
     var body: some View {
-        HStack {
-            Image("jsoundo")
-                .resizable()
-                .scaledToFit()
-            HStack {
-                Text("John Soundo")
-                    .font(.headline)
-                Text("@jsoundo")
-                    .font(.caption)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 10) {
+                    HStack(spacing: 50){
+                        Image("timothy")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        HStack(spacing: 6) {
+                            UserStatView(value: 3, title: "Posts")
+                            UserStatView(value: 8, title: "Followes")
+                            UserStatView(value: 34, title: "Following")
+                        }
+                    }
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Chadwick Bozeman")
+                            .fontWeight(.semibold)
+                        Text("Wakanda Forever")
+                        
+                    }
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.secondary, lineWidth: 1)
+                            )
+                    }
+                    
+                    Divider()
+                    
+                }
+                .padding()
+                
+                LazyVGrid(columns: gridItem, spacing: 1) {
+                    ForEach(0..<15, id: \.self) { _ in
+                        Image("timothy")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button(action: { }) {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
             }
         }
     }
--- Instagram/Core/Profile/UserStatView.swift
+++ Instagram/Core/Profile/UserStatView.swift
@@ -1,18 +1,20 @@
-//
-//  UserStatView.swift
-//  Instagram
-//
-//  Created by Almaty on 30.06.2025.
-//
-
 import SwiftUI
 
 struct UserStatView: View {
+    let value: Int
+    let title: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            Text(title)
+                .font(.footnote)
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
     }
 }
 
 #Preview {
-    UserStatView()
+    UserStatView(value: 3, title: "Posts")
 }
